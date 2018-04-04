40c357ee484010cef978fe639efbbe0d700dd9ef
Admin <Admin@gmail.com>
2018-04-04 14:59:10 -0700

    fix BandA

--- features/step_definitions/before_and_after.rb
+++ features/step_definitions/before_and_after.rb
@@ -3,6 +3,13 @@
 # local variable
 driver_path_darwin_mozilla = "/Users/admin/Documents/DevS-iOS/tools/selenium/geckodriver/mozilla/geckodriver-v0.20/geckodriver"
 driver_path_darwin_chrome = "/Users/admin/Documents/DevS-iOS/tools/selenium/geckodriver/chrome/chromedriver-v2.37/chromedriver"
+
+driver_path_windows_mozilla = "/Users/admin/Documents/DevS-iOS/tools/selenium/geckodriver/mozilla/geckodriver-v0.20/geckodriver.exe"
+driver_path_windows_chrome = "/Users/admin/Documents/DevS-iOS/tools/selenium/geckodriver/chrome/chromedriver-v2.37/chromedriver.exe"
+
+driver_type = "chrome"
+# driver_type = "firefox"
+
 url = "http://www.practicalsqa.net/"
 
 # instance variable
@@ -15,16 +22,21 @@ url = "http://www.practicalsqa.net/"
 Before do
   puts "run method 'Before'"
 
-  # puts Launchy::Application.new.host_os_family # => :windows, :darwin, :nix, or :cygwin
+  # puts Launchy::Application.new.host_os_family.to_s # => :windows, :darwin, :nix, or :cygwin
   if Launchy::Application.new.host_os_family.to_s.include? ":Darwin"
-    driver_path = driver_path_darwin_mozilla
+    if driver_type == "chrome"
+      @browser = Selenium::WebDriver.for :chrome, driver_path: driver_path_darwin_chrome
+    else
+      @browser = Selenium::WebDriver.for :firefox, driver_path: driver_path_darwin_mozilla
+    end
   elsif Launchy::Application.new.host_os_family.to_s.include? ":Windows"
-    driver_path = driver_path_darwin_chrome
+    if driver_type == "chrome"
+      @browser = Selenium::WebDriver.for :chrome, driver_path: driver_path_windows_chrome
+    else
+      @browser = Selenium::WebDriver.for :firefox, driver_path: driver_path_windows_mozilla
+    end
   end
 
-
-  # $browser = Selenium::WebDriver.for :firefox
-  @browser = Selenium::WebDriver.for :chrome, driver_path: driver_path_darwin_chrome
   @browser.manage.window.maximize
   @browser.manage.timeouts.implicit_wait = 10
   @browser.manage.timeouts.page_load =10
