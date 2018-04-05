cc9249d057997130376db32c840c9f9dbe21da1a
lara12 <lbruter@yahoo.com>
2018-04-04 22:13:20 -0700

    adding features folder

---
+++ features/ContactMe.feature
@@ -0,0 +1,12 @@
+
+Feature: Contactme
+  @acceptance
+  Scenario: Leaving a Comment
+    Given I navigate to Contact Me page
+    When I verify the Contact Me page loaded
+    Then I write my comment "Hello world! This is Selenium test."
+    And I input my name "John Smith"
+    Then I input my email address "my.email@gmail.com"
+    And I input my web site url "www.my_web_url.com"
+    Then I check subscribe checkbox
+    And I click on Post Comment button
---
+++ features/support/env.rb
@@ -0,0 +1,19 @@
+require "selenium-webdriver"
+require "rspec"
+
+driver_path =  File.join(File.dirname(__FILE__), "..", "..", "drivers","chromedriver.exe")
+puts  driver_path
+
+
+ Before do
+   @browser = Selenium::WebDriver.for :chrome, driver_path: driver_path
+   @browser.manage.timeouts.implicit_wait = 10
+   @browser.manage.timeouts.page_load =10
+  # $base_url = "practicalsqa.net"
+  # @browser.get "http://www.#{$base_url}"
+
+end
+
+After do
+  @browser.quit
+end
